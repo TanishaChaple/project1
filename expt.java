50c96aa2a18adc526c336f1264ffadacdff70463
TanishaChaple <tanishachaple@gmail.com>
2025-04-25 13:03:15 -0300

    Create expt.java

---
+++ expt.java
@@ -0,0 +1,10 @@
+public class myfirstclass
+{
+  public static void main(String [] args)
+  {
+     System.out.println("Hello")
+     System.out.println("Java")
+     System.out.println("Programming")
+     System.out.println()
+   }
+}
